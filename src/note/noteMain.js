2b742d5524ce800b523f2c119f9faaae61ae0b89
C0ldHands <48500351+C0ldHands@users.noreply.github.com>
2020-12-28 13:34:29 -0500

    Changed date formating

--- src/note/noteMain.js
+++ src/note/noteMain.js
@@ -7,13 +7,17 @@ function NoteMain(props){
   const note = STORE.notes.find(note =>
     note.id ===  selectedNoteID
   )
+  const date = new Date(note.modified);
+  const year = date.getFullYear();
+  const month = date.getMonth()+1;
+  const day = date.getDate();
 
   return(
     <>
       <div id = 'noteContainer'>
         <h3>{note.name}</h3>
         <div id = 'date-button-box'>
-          <p>Date modified: {note.modified}</p>
+          <p>Date modified: {`${month}/${day}/${year}`}</p>
           <button id = 'deleteNoteBtn'>Delete Note</button>
         </div> 
       </div>
