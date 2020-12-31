ee7d3e8b51cb193d20fee745eb5e5736f1764bd1
C0ldHands <48500351+C0ldHands@users.noreply.github.com>
2020-12-31 08:33:54 -0500

    Replaced .map with .filter

--- src/folder/folderMain.js
+++ src/folder/folderMain.js
@@ -9,42 +9,42 @@ function FolderMain(props){
   return(
     <NotefulContext.Consumer>
       {(value) => {
+        const selectedNotes = value.notes.filter(note => note.folderId === selectedFolderID)
         return(
           <>
             <ul>
-              {value.notes.map(note => {
+              {selectedNotes.map(note => {
                 const date = new Date(note.modified);
                 const year = date.getFullYear();
                 const month = date.getMonth()+1;
                 const day = date.getDate();
-                if (note.folderId === selectedFolderID){
-                  return(
-                    <li key = {note.id}>
-                      <div id = 'noteContainer'>
-                        <h3><Link to ={`/note/${note.id}`}>{note.name}</Link></h3>
-                        <div id = 'date-button-box'>
-                          <p>Date modified: {`${month}/${day}/${year}`}</p>
-                          <button 
-                            id = {note.id} 
-                            className = 'deleteNoteBtn'
-                            onClick = {(e)=>{
-                              fetch(`http://localhost:9090/notes/${e.target.id}`, {
-                                method: 'DELETE',
-                                headers: {
-                                  'content-type': 'application/json'
-                                },
-                              })
-                              value.handleDeleteNote(e.target.id);
-                            }}
-                          >
-                            Delete Note
-                          </button>
-                        </div>
+                return(
+                  <li key = {note.id}>
+                    <div id = 'noteContainer'>
+                      <h3><Link to ={`/note/${note.id}`}>{note.name}</Link></h3>
+                      <div id = 'date-button-box'>
+                        <p>Date modified: {`${month}/${day}/${year}`}</p>
+                        <button 
+                          id = {note.id} 
+                          className = 'deleteNoteBtn'
+                          onClick = {(e)=>{
+                            fetch(`http://localhost:9090/notes/${e.target.id}`, {
+                              method: 'DELETE',
+                              headers: {
+                                'content-type': 'application/json'
+                              },
+                            })
+                            value.handleDeleteNote(e.target.id);
+                          }}
+                        >
+                          Delete Note
+                        </button>
                       </div>
-                    </li>
-                  )
-                }
+                    </div>
+                  </li>
+                )
               })}
+              
             </ul>
             <button id = 'addNoteBtn'>Add note</button>
           </>
