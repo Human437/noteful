424188f3fc8d3d3413e3a4587013e24c6d4bedd8
Ryan <leeyuenhigh@gmail.com>
2021-01-15 02:28:47 -0500

    Resolved type issue for delete button

--- src/App.js
+++ src/App.js
@@ -44,7 +44,7 @@ class App extends React.Component {
 
   handleDeleteNote = (noteId) => {
     this.setState({
-        notes: this.state.notes.filter(note => note.id !== noteId)
+        notes: this.state.notes.filter(note => Number(note.id) !== Number(noteId))
     });
   }
 
