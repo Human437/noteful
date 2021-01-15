61c6a9d7a906b484205d1234f03e935b8bb2246e
Ryan <leeyuenhigh@gmail.com>
2021-01-15 01:57:19 -0500

    Added config file to access sensitive info

---
+++ src/config.js
@@ -0,0 +1,6 @@
+// eslint-disable-next-line
+export default {
+    BEARER_TOKEN: process.env.REACT_APP_BEARER_TOKEN,
+    API_FOLDER_ENDPOINT: process.env.REACT_APP_API_FOLDER_ENDPOINT,
+    API_NOTES_ENDPOINT: process.env.REACT_APP_API_NOTES_ENDPOINT
+}
